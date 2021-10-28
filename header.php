df20398d278505906a11a6b8cc231951d07d4053
sudharshan-hashroot <sudharshan.m@hashroot.com>
2021-10-28 16:02:06 +0530

    dynmaic other service options

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 }
 </style>
   
-    <div class="topbar topbar-dark bg-dark" style="z-index:100">
+    <div class="topbar topbar-dark bg-dark" style="z-index:99">
       <div class="container d-md-flex align-items-center px-0 px-xl-3">
         <div class="d-none d-md-block text-nowrap me-3"><i class="ai-phone fs-base text-muted me-1 align-middle"></i><span class="text-muted me-2">Support</span><a class="topbar-link me-1" href="tel:+919778426320">+91 9778426320</a></div>
         <div class="d-flex text-md-end ms-md-auto">
@@ -33,7 +33,7 @@
     <div class="navbar navbar-expand-lg navbar-light bg-light navbar-shadow navbar-sticky" data-scroll-header="" data-fixed-element="" style="z-index:99">
      
       <div class="container px-0 px-xl-3">
-        <button class="navbar-toggler ms-n2 me-4 d-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#primaryMenu"><span class="navbar-toggler-icon"></span></button><a class="navbar-brand flex-shrink-0 order-lg-1 mx-auto ms-lg-0 pe-lg-2 me-lg-4" href="/"><img class="d-none d-lg-block" src="img/logo/logo-dark.svg" alt="Around" width="153"><img class="d-lg-none" src="img/logo/logo-icon.svg" alt="Around" width="58"></a>
+        <button class="navbar-toggler ms-n2 " style="z-index: 100;" type="button" data-bs-toggle="offcanvas" data-bs-target="#primaryMenu"><span class="navbar-toggler-icon"></span></button><a class="navbar-brand flex-shrink-0 order-lg-1 mx-auto ms-lg-0 pe-lg-2 me-lg-4" href="/"><img class="d-none d-lg-block" src="img/logo/logo-dark.svg" alt="Around" width="153" style="margin-right:3rem;"><img class="d-lg-none" style="margin-right:3rem;" src="img/logo/logo-icon.svg" alt="Around" width="58"></a>
         <div class="d-flex align-items-center order-lg-3 ms-lg-auto d-none"><a class="nav-link-style fs-sm text-nowrap" href="#modal-signin" data-bs-toggle="modal" data-view="#modal-signin-view"><i class="ai-user fs-xl me-2 align-middle"></i>Sign in</a><a class="btn btn-primary ms-grid-gutter d-none d-lg-inline-block" href="#modal-signin" data-bs-toggle="modal" data-view="#modal-signup-view">Sign up</a></div>
         <div class="offcanvas offcanvas-collapse order-lg-2" id="primaryMenu">
           <div class="offcanvas-header navbar-shadow">
