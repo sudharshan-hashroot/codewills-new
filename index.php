1af0449565db4fd722704e8cc0adcbacdba31b04
Akhils <akhil.s@vandalayglobal.com>
2021-10-29 15:37:27 +0530

    popup added

--- index.php
+++ index.php
@@ -167,55 +167,8 @@
     <?php include("signinmodal.php"); ?>
     <?php include("header.php"); ?>
     <!-- Enquirenow Modal -->
-    <div class="modal fade" id="modal-enquirenow" tabindex="-1">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content border-0">
-          <div class="view show" id="modal-enquirenow-view">
-            <div class="modal-header border-0 bg-dark px-4">
-              <h4 class="modal-title text-light">Enquire Now</h4>
-              <button class="btn-close btn-close-white" type="button" data-bs-dismiss="modal"
-                aria-label="btn-close "></button>
-            </div>
-            <div class="modal-body px-4">
-              <p class="fs-ms text-muted">Please Provide following details</p>
-              <form class="needs-validation"  method="post" id="enquire_form"
-                        novalidate="novalidate" action="#" enctype="multipart/form-data" novalidate>
-                <div class="mb-3">
-                  <div class="input-group"><i
-                      class="ai-user position-absolute top-50 start-0 translate-middle-y ms-3"></i>
-                    <input class="form-control rounded" type="text"  name="r_name"  id="r_name"  placeholder="Your Name" required>
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <div class="input-group"><i
-                      class="ai-mail position-absolute top-50 start-0 translate-middle-y ms-3"></i>
-                    <input class="form-control rounded" type="email"  name="r_email"  id="r_email"  placeholder="Your E-mail" required>
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <div class="input-group"><i
-                      class="ai-phone position-absolute top-50 start-0 translate-middle-y ms-3"></i>
-                    <input class="form-control rounded" type="tel"  name="r_phone"  id="r_phone"  placeholder="Your Mobile Number" oninput="this.value = this.value.replace(/[^0-9.+\s]/g, '').replace(/(\..*)\./g, '$1');" required>
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <div class="input-group">
-                    <i class="ai-message-square position-absolute start-0 translate-middle-y ms-3"
-                      style="top: 15%!important;"></i>
-                    <textarea rows='5' class="form-control rounded" type="text"  name="r_message"  id="r_message"  placeholder="Your Message"
-                      required></textarea>
-                  </div>
-                </div>
+    <?php include("enquirePopup.php"); ?>
 
-                <button class="btn btn-primary d-block w-100" type="submit">Enquire Now</button>
-
-              </form>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
     <!-- Page content-->
     <!-- Hero - Featured Products (tabs)-->
     <section class="position-relative bg-gradient-home-slider pt-5 pt-lg-6 pb-4 pb-lg-5">
